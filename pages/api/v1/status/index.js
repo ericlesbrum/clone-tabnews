a8a6a91700297247aa0263d602b96efaa2a35381
Ericles Brum <ericlespbrum@gmail.com>
2024-06-21 19:29:27 +0000

    Finaliza `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "servidor ok" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW MAX_CONNECTIONS;");
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: databaseVersionResult.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
